1ff5e4d510d9ca781bb52c9004c2c4145d1667ba
edmilton.peixeira <edmilton.peixeira@equilibriumweb.com>
2024-01-20 10:30:57 -0300

    Commit inicial

---
+++ backend/src/main/java/com/dev/edmilton/workdev/WorkdevApplication.java
@@ -0,0 +1,13 @@
+package com.dev.edmilton.workdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkdevApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dev/edmilton/workdev/WorkdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.edmilton.workdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

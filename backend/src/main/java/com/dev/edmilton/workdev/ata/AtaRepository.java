c6a95b8a19b9c4d9afb1ebc14b7a61107e94f9fa
EdmiltonPeixeira <edm.ufpasi@gmail.com>
2024-01-20 14:39:51 -0300

    (feat): criando classes repository para cada entidade

---
+++ backend/src/main/java/com/dev/edmilton/workdev/ata/AtaRepository.java
@@ -0,0 +1,6 @@
+package com.dev.edmilton.workdev.ata;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtaRepository extends JpaRepository<Ata, Integer> {
+}
---
+++ backend/src/main/java/com/dev/edmilton/workdev/workshop/WorkshopRepository.java
@@ -0,0 +1,6 @@
+package com.dev.edmilton.workdev.workshop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkshopRepository extends JpaRepository<Workshop, Integer> {
+}
